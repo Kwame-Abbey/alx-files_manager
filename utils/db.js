0cc10fbc67e4616609613bf0a7f7b0ce5cabb54f
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-07-03 20:36:04 +0100

    Create a new user

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
+const mongo = require('mongodb');
+const { passwdHashed } = require('./hashPassword');
 
 class DBClient {
   constructor() {
@@ -35,6 +37,40 @@ class DBClient {
       .collection('files')
       .countDocuments();
     return count;
+  }
+
+  async createUser(email, password) {
+    const hashedpasswd = passwdHashed(password);
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').insertOne({ email, password: hashedpasswd });
+    return user;
+  }
+
+  async getUser(email) {
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').find({ email }).toArray();
+    if (!user.length) {
+      return null;
+    }
+    return user[0];
+  }
+
+  async getUserById(id) {
+    const _id = new mongo.ObjectID(id);
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').find({ _id }).toArray();
+    if (!user.length) {
+      return null;
+    }
+    return user[0];
+  }
+
+  async userExist(email) {
+    const user = await this.getUser(email);
+    if (user) {
+      return true;
+    }
+    return false;
   }
 }
 
